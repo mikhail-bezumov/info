e4b2190e3304e5ebfc06e4defe3c0b43c7da5b37
mikhail-bezumov <mikhail.bezumov@gmail.com>
2025-04-02 22:01:54 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+//const firstName = 'Mikhail'
+//firstName = 'Ivan'
+//console.log('hello', firstName)
+let firstName = 'mikhail'
+firstName = 'Ivan'
+console.log('hello', firstName)
+git config --global user.name "Mikhail_Bezumov"
+git config --global user.email "mikhail.bezumov@gmail.com"
